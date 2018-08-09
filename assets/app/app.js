c35dce8af4561223a04cccb9943ddad50bede7f0
vvanslee <vvanslee@gmail.com>
2018-08-08 20:59:39 -0500

    added pages and links to them

---
+++ assets/app/app.js
@@ -0,0 +1,8 @@
+
+// Randomly selects background color
+
+// $( document ).ready(function() {
+//     var bgcolorlist=new Array("silver", "#BAF3C3", "#c3baf3")
+    
+//     $("body").css("background-color",bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]);
+// });
